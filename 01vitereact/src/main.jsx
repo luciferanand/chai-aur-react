43c9f5419aecf97a8887d8a157cb13942c489866
luciferanand <chandraanand2003@gmail.com>
2026-08-18 22:36:19 +0530

    making own jsx element

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
+// import Chai from './chai.jsx'
 import App from './App.jsx'
 
+function MyApp(){
+    return (
+        <div>
+            <a href="https://google.com"></a>
+        </div>
+    )
+}
+
+const anotherElement=(
+    <a href="https://google.com" target='_blank'>visit google</a>
+)
+
+const reactElement=React.createElement(
+    'a',
+    {href:"https://google.com",target:'_blank'},
+    'click me to visit google'
+)
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  
+    <App/>
+
 )
